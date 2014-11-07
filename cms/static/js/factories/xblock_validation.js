fa70c73a6a3222d845aafb2c9a011237ad593ffe
cahrens <christina@edx.org>
2014-11-07 11:53:02 -0500

    Use require, not define.

--- cms/static/js/factories/xblock_validation.js
+++ cms/static/js/factories/xblock_validation.js
@@ -1,22 +1,22 @@
 define(["jquery", "js/views/xblock_validation", "js/models/xblock_validation"],
 function ($, XBlockValidationView, XBlockValidationModel) {
     'use strict';
-    return function (validationMessages, xblock_url, is_root) {
-       // % if xblock_url and not is_root:
-       // validationMessages.showSummaryOnly = true;
+    return function (validationMessages, xblock_location, xblock_url, is_root) {
+        if (xblock_url && !is_root) {
+            validationMessages.showSummaryOnly = true;
+        }
+
         var model = new XBlockValidationModel(validationMessages, {parse: true});
 
         if (!model.get("empty")) {
-            var validationEle = $('div.xblock-validation-messages[data-locator="${xblock.location | h}"]');
-            var viewOptions = {
-                el: validationEle,
-                model: model
-            };
-            //% if is_root:
-          //  viewOptions.root = true;
+            var validationEle = $('div.xblock-validation-messages[data-locator="' + xblock_location + '"]');
+            var viewOptions = {el: validationEle, model: model, root: is_root};
 
-            var view = new XBlockValidationView(viewOptions);
-            view.render();
+            if (is_root) {
+                viewOptions.root = true;
+            }
+
+            new XBlockValidationView(viewOptions).render();
         }
     };
 });
