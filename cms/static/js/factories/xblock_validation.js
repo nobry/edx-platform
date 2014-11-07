5bf52d31b79e7c88cd8ce6154074845acc11d6eb
cahrens <christina@edx.org>
2014-11-07 15:59:02 -0500

    Get rid of JQuery dependency.

--- cms/static/js/factories/xblock_validation.js
+++ cms/static/js/factories/xblock_validation.js
@@ -1,7 +1,7 @@
-define(["jquery", "js/views/xblock_validation", "js/models/xblock_validation"],
-function ($, XBlockValidationView, XBlockValidationModel) {
+define(["js/views/xblock_validation", "js/models/xblock_validation"],
+function (XBlockValidationView, XBlockValidationModel) {
     'use strict';
-    return function (validationMessages, xblock_location, xblock_url, is_root) {
+    return function (validationMessages, xblock_url, is_root, validationEle) {
         if (xblock_url && !is_root) {
             validationMessages.showSummaryOnly = true;
         }
@@ -9,7 +9,6 @@ function ($, XBlockValidationView, XBlockValidationModel) {
         var model = new XBlockValidationModel(validationMessages, {parse: true});
 
         if (!model.get("empty")) {
-            var validationEle = $('div.xblock-validation-messages[data-locator="' + xblock_location + '"]');
             var viewOptions = {el: validationEle, model: model, root: is_root};
 
             if (is_root) {
