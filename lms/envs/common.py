15931bf5072345174908982e5df83767d3c67ff8
Oper Edulib <oper.edulib@gmail.com>
2016-02-01 16:35:32 -0500

    Putting comments around tracking logs consent (might revive later on)

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2163,7 +2163,7 @@ REGISTRATION_EXTRA_FIELDS = {
     'terms_of_service': 'hidden',
     'city': 'hidden',
     'country': 'hidden',
-    'consent': 'required',
+####    'consent': 'required',
 }
 
 ########################## CERTIFICATE NAME ########################
